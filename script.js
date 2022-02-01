2d0078c0b439afedf48dc5441c55738fc1e90443
Apokatastasis <ramirolonatti@gmail.com>
2022-01-31 23:39:06 -0300

    Added sum, substraction, multiplication and divition functions

---
+++ script.js
@@ -0,0 +1,165 @@
+let displayValue = null;
+let secondOperator = null;
+let operation = null;
+let operationBackup = null;
+let result = null;
+let wasScreenCleared = true;
+let wasEqualPressed = false;
+let writeOperator;
+
+
+
+
+
+const numberButtons = document.querySelectorAll(".num");
+const displayLowerScreen = document.querySelector(".bottom");
+const displayUpperScreen = document.querySelector(".upper");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalBtn = document.querySelector(".equalsBtn");
+const clearBtn = document.querySelector(".clear");
+
+//clean screen when pressing
+numberButtons.forEach(button=> button.addEventListener("click", displayNumbers));
+
+operatorButtons.forEach(button => button.addEventListener('click',(e)=> {
+    
+    //when an operator is pressed the number on the screen is saved on displayValue, we are going to use it as our first operand.
+    //if the screen was clearead it means that the first value has  already been saved 
+    if(!wasScreenCleared) displayValue = +displayLowerScreen.textContent;
+    
+    writeOperator = e.target.value;
+    operation = e.target.value;
+    
+    if(operationBackup === null) operationBackup = operation;
+    //if (wasEqualPressed) operationBackup = operation;
+    evaluate(operationBackup);
+    //evaluate(operation)
+}));
+
+
+
+equalBtn.addEventListener("click", ()=>{
+    if(displayValue === null) return;
+    secondOperator = +displayLowerScreen.textContent;
+    result = operate(operationBackup, displayValue, secondOperator);
+    printResult(operationBackup);
+    //operation = operationBackup;
+    displayValue = result;
+    secondOperator = null;
+    wasScreenCleared = false;
+    wasEqualPressed = true;
+    
+
+})
+
+
+clearBtn.addEventListener('click', clear);
+
+
+
+
+function evaluate(operation){
+    //if the screen was cleared it's because an operator was pressed so we don't store the number on the screen in "displayValue", 
+    //because displayValue (our first operand) is stored, instead the number on the screen is stored in secondOperator.
+    if (wasScreenCleared) secondOperator = +displayLowerScreen.textContent;
+   
+    result = operate(operation, displayValue, secondOperator);
+    printResult(operation);
+    //operation = null;
+    displayValue = +displayLowerScreen.textContent;
+    //result = displayValue;
+    
+    secondOperator = null;
+    wasScreenCleared = false;
+    
+    
+    
+    
+    
+
+}
+
+
+function printResult(operation){
+    console.log(displayValue, secondOperator);
+    if(displayValue!==null && secondOperator!==null){
+        console.log(secondOperator);
+        displayUpperScreen.textContent = `${displayValue} ${operationBackup} ${secondOperator} =`;
+        displayLowerScreen.textContent = result;
+    } else if(secondOperator===null){
+        displayUpperScreen.textContent = `${displayValue} ${writeOperator} `;
+
+    } else{
+        displayUpperScreen.textContent = `${secondOperator} ${operation}`;
+    }
+    
+}
+
+
+
+function clear(){
+    displayLowerScreen.textContent ="";
+    displayUpperScreen.textContent="";
+    displayValue = null;
+    secondOperator = null;
+    
+    operation = null;
+    operationBackup = null;
+    result = null;
+    wasScreenCleared = false;
+    
+}
+
+function clearScreen(){
+    wasScreenCleared = true;
+    displayLowerScreen.textContent ="";
+    displayUpperScreen.textContent="";
+    
+}
+
+
+function displayNumbers(e){
+    
+    if(!wasScreenCleared){
+
+        clearScreen();
+        
+        wasScreenCleared = true;
+        displayLowerScreen.textContent += e.target.value;
+        operationBackup = operation;
+       
+        
+        
+    } else{
+        
+        
+        displayLowerScreen.textContent += e.target.value;
+        
+    }
+
+}
+
+
+
+function add(a, b){
+    return a+b;
+}
+function substract(a,b){
+    return a - b;
+}
+function multiply(a,b){
+    return a * b;
+}
+function divide(a,b){
+    return a / b;
+
+}
+
+function operate(operator, num1, num2){
+    
+    if(operator==="+") return add(num1,num2);
+    if (operator==="-") return substract(num1,num2);
+    if(operator==="x") return multiply(num1,num2);
+    if(operator==="÷") return divide(num1,num2);
+
+}
